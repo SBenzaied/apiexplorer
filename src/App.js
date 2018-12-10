e0c24d91b8add51b9db86825f28a4e92a6225e47
romaric roussel <romaric.roussel@outlook.fr>
2018-12-10 16:49:41 +0100

    Fonction de recherche du nom du hero qui commence par la recherche

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Suggestions from './Suggestion'
+import axios from 'axios'
 
-class App extends Component {
+const API_KEY = "055211f04433158edb47811e0135b554";
+const HASH = "de858e129a6d31c17bc1157985f52817"
+const API_URL = 'http://gateway.marvel.com/v1/public/characters'
+
+class App extends React.Component {
+  state = {
+    query: '',
+    results: []
+  }
+
+  getInfo = () => {
+    axios.get(`${API_URL}?nameStartsWith=${this.state.query}&ts=1&apikey=${API_KEY}&hash=${HASH}`)
+      .then(({ data }) => {
+        this.setState({
+          results: data.data.results 
+                                                     
+        })
+      })
+  }
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if (this.state.query && this.state.query.length > 1) {
+        if (this.state.query.length % 2 === 0) {
+          this.getInfo()
+        }
+      } 
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App">   
+      <form>
+        <input
+          placeholder="Search for..."
+          ref={input => this.search = input}
+          onChange={this.handleInputChange}
+        />
+        <Suggestions results={this.state.results} />
+      </form>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
+
