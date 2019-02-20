fe3bee50848ac5ee866c185228ca7944a85b7d65
Unknown <sof.benzaied@gmail.com>
2019-02-20 15:15:57 +0100

    align images

--- src/Suggestion.js
+++ src/Suggestion.js
@@ -19,34 +19,34 @@ const Suggestions = (props) => {
     options = props.results.map(r => (
       
       <li key={r.id}>
-      <Link to={{pathname : "/characters", state:{name:r.name,description:r.description,image:r.thumbnail.path+imgBig}}}> {r.name} <img class ="images" src={r.thumbnail.path + img} alt="" /></Link></li>
+      <Link to={{pathname : "/characters", state:{name:r.name,description:r.description,image:r.thumbnail.path+imgBig}}}> {r.name} <div class="container"> <img class ="images" src={r.thumbnail.path + img} alt="" /></div></Link></li>
      ))
 
     break
     case "comics":
     options = props.results.map(r => (
       <li key={r.id}>
-      <Link to={{pathname : "/comics", state:{title:r.title,nbPage:r.pageCount,image:r.thumbnail.path+imgBig,description:r.description}}}> {r.title} <img class ="images" src={r.thumbnail.path + img} alt="" /></Link></li>
+      <Link to={{pathname : "/comics", state:{title:r.title,nbPage:r.pageCount,image:r.thumbnail.path+imgBig,description:r.description}}}> {r.title} <div class="container"><img class ="images" src={r.thumbnail.path + img} alt="" /></div></Link></li>
       ))
       break
     case "series":
     options = props.results.map(r => (
       <li key={r.id}>
        <Link to={{pathname : "/series", state:{title:r.title,description:r.description,image:r.thumbnail.path+imgBig,startYear:r.startYear,
-       endYear:r.endYear,rating:r.rating,type:r.type}}}> {r.title} <img class ="images" src={r.thumbnail.path + img} alt="" /></Link></li>
+       endYear:r.endYear,rating:r.rating,type:r.type}}}> {r.title} <div class="container"><img class ="images" src={r.thumbnail.path + img} alt="" /></div></Link></li>
       ))
       break
     case "events":
     options = props.results.map(r => (
       <li key={r.id}>
       <Link to={{pathname : "/events", state:{title:r.title,description:r.description,image:r.thumbnail.path+imgBig,start:r.start,
-       end:r.end}}}> {r.title} <img class ="images" src={r.thumbnail.path + img} alt="" /></Link></li>
+       end:r.end}}}> {r.title} <div class="container"><img class ="images" src={r.thumbnail.path + img} alt="" /></div></Link></li>
         ))
     break
     case "creators":
       options = props.results.map(r => (
         <li key={r.id}>
-         <Link to={{pathname : "/creators", state:{name:r.fullName,image:r.thumbnail.path+imgBig}}}> {r.fullName} <img class ="images" src={r.thumbnail.path + img} alt="" /></Link></li>))
+         <Link to={{pathname : "/creators", state:{name:r.fullName,image:r.thumbnail.path+imgBig}}}> {r.fullName} <div class="container"> <img class ="images" src={r.thumbnail.path + img} alt="" /></div></Link></li>))
       break
   }
   return <ul>{options}</ul>
